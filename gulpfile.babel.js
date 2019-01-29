baa7b16d97ed2aef3175cf72b2435c5fb7b3bf7d
Robin Giese <robin@sarahandrobin.com>
2019-01-29 15:58:30 -0800

    Moving to Gulp 4

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,41 +6,25 @@ import del from "del";
 import hugoBin from "hugo-bin";
 
 import autoprefixer from "gulp-autoprefixer";
-import gutil from "gulp-util";
-import flatten from "gulp-flatten";
 import hash from "gulp-hash";
 import sass from "gulp-sass";
 import newer from "gulp-newer";
 import responsive from "gulp-responsive";
 import imagemin from "gulp-imagemin";
 
-import webpack from "webpack";
+import webpack from "webpack-stream";
 import webpackConfig from "./webpack.conf";
 
 import BrowserSync from "browser-sync";
-
+import { doesNotReject } from "assert";
 const browserSync = BrowserSync.create();
 
-// Hugo arguments
-const hugoArgsDefault = ["-d", "../dist", "-s", "site", "-v"];
-const hugoArgsPreview = ["--buildDrafts", "--buildFuture"];
-
-const hugoDependencies = ["css", "js", "fonts", "img"];
-
-// Build tasks
-gulp.task("build-prod", hugoDependencies, (cb) => buildSite(cb, [], "production"));
-gulp.task("build-prod-preview", hugoDependencies, (cb) => buildSite(cb, hugoArgsPreview, "production"));
-
-gulp.task("build-dev", hugoDependencies, (cb) => buildSite(cb));
-gulp.task("build-dev-preview", hugoDependencies, (cb) => buildSite(cb, hugoArgsPreview));
-
-// Run server tasks
-gulp.task("server", ["build-dev"], (cb) => runServer(cb));
-gulp.task("server-preview", ["build-dev-preview"], (cb) => runServer(cb));
-
+//
+// Tasks
+//
 
 // Compile CSS with SASS
-gulp.task("css", () => {
+function compileCSS() {
   del("./dist/assets/css/**/*");
 
   return gulp.src("./src/sass/**/*.scss")
@@ -51,35 +35,20 @@ gulp.task("css", () => {
     .pipe(hash.manifest("hash.json"))
     .pipe(gulp.dest("./site/data/generated"))
     .pipe(browserSync.stream());
-});
+}
 
 // Compile Javascript
-gulp.task("js", (cb) => {
-  const myConfig = Object.assign({}, webpackConfig);
-
-  return webpack(myConfig, (err, stats) => {
-    if (err) throw new gutil.PluginError("webpack", err);
-    gutil.log("[webpack]", stats.toString({
-      colors: true,
-      progress: true
-    }));
-    browserSync.reload();
-    cb();
-  });
-});
-
-// Move all fonts in a flattened directory
-gulp.task('fonts', () => {
-  return gulp.src("./src/fonts/**/*")
-    .pipe(flatten())
-    .pipe(gulp.dest("./dist/fonts"))
+function compileJS() {
+  return gulp.src("./src/js/app.js")
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest("dist/"))
     .pipe(browserSync.stream());
-});
+}
 
 // Resize images to responsive sizes
-gulp.task('img', () => {
+function resizeImages() {
   return gulp.src("./src/img/**/*")
-    .pipe(newer("./dist/img"))
+    .pipe(newer("./dist/img-original"))
     .pipe(gulp.dest("./dist/img-original"))
     .pipe(responsive({
       "**/*": [{
@@ -106,7 +75,7 @@ gulp.task('img', () => {
       imagemin.svgo()
     ]))
     .pipe(gulp.dest("./dist/img"));
-});
+}
 
 // Development server with browsersync
 function runServer() {
@@ -115,19 +84,20 @@ function runServer() {
       baseDir: "./dist"
     }
   });
-  gulp.watch("./src/js/**/*.js", ["js"]);
-  gulp.watch("./src/sass/**/*.scss", ["css"]);
-  gulp.watch("./src/fonts/**/*", ["fonts"]);
-  gulp.watch("./site/**/*", ["build-dev"]);
+
+  gulp.watch("./src/js/**/*.js", compileJS);
+  gulp.watch("./src/sass/**/*.scss", compileCSS);
+  gulp.watch(["./site/**/*", "!./site/data/generated/*"], exports.build);
 };
 
-/**
- * Run hugo and build the site
- */
-function buildSite(cb, options, environment = "development") {
+// Build site with Hugo
+const hugoArgsDefault = ["-d", "../dist", "-s", "site", "-v"];
+const hugoArgsPreview = ["--buildDrafts", "--buildFuture"];
+
+function buildSite(cb, options) {
   const args = options ? hugoArgsDefault.concat(options) : hugoArgsDefault;
 
-  process.env.NODE_ENV = environment;
+  process.env.NODE_ENV = "production";
 
   return spawn(hugoBin, args, {stdio: "inherit"}).on("close", (code) => {
     if (code === 0) {
@@ -139,3 +109,26 @@ function buildSite(cb, options, environment = "development") {
     }
   });
 }
+
+function buildSite_Production(cb) {
+  return buildSite(cb, []);
+}
+
+function buildSite_ProductionPreview(cb) {
+  return buildSite(cb, hugoArgsPreview);
+}
+
+
+//
+// Assembling it all
+//
+
+// Build tasks
+const hugoDependencies = gulp.parallel(compileCSS, compileJS, resizeImages);
+
+exports.build = gulp.series(hugoDependencies, buildSite_Production);
+exports.build_preview = gulp.series(hugoDependencies, buildSite_ProductionPreview);
+
+// Run server tasks
+exports.server = gulp.series(exports.build, runServer);
+exports.server_preview = gulp.series(exports.build_preview, runServer);

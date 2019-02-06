1650ef63a90da635057c1aa343d8a587aefc1a29
Robin Giese <robin@sarahandrobin.com>
2019-02-05 21:41:22 -0800

    Improve 404 page.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,30 @@
+import { Link } from "gatsby";
 import React from "react";
 
+import Icon from "../components/icon";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+
+import SadTurnip from "../assets/icons/sad-turnip.svg";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Icon sprite={SadTurnip} className="h4 w4" />
+    <p>You just hit a page that doesn't exist... such sadness.</p>
+    <p>
+      Try heading{` `}
+      <Link className="link accent" to="/">
+        home
+      </Link>
+      {` `}
+      or to the{` `}
+      <Link className="link accent" to="/posts/all">
+        post index
+      </Link>
+      .
+    </p>
   </Layout>
 );
 

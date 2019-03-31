d9936db920a8b039c16eae6644f98cd288f6b00e
Robin Giese <robin@grumpycorp.com>
2019-03-30 20:16:16 -0700

    Retire old static image directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,13 +78,6 @@ module.exports = {
     },
 
     // Image processing pipeline
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
 

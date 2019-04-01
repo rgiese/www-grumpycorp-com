eed2a5491e6ef4d155bbb132729d0c86f3350d17
Robin Giese <robin@grumpycorp.com>
2019-03-31 19:43:35 -0700

    Simplify portfolio page content - just the content, basically

--- src/templates/portfolio.tsx
+++ src/templates/portfolio.tsx
@@ -1,13 +1,10 @@
-import { graphql, Link } from "gatsby";
+import { graphql } from "gatsby";
 // tslint:disable-next-line no-submodule-imports
 import MDXRenderer from "gatsby-mdx/mdx-renderer";
 import React from "react";
 
-import Icon from "../components/icon";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-
-import TagIcon from "../assets/icons/tag.svg";
 
 // Page context to be provided from ../gatsby/createPages.ts
 export interface IPortfolioPageContext {
@@ -23,7 +20,6 @@ export const pageContentQuery = graphql`
         body
       }
       frontmatter {
-        tags
         title
       }
     }
@@ -37,7 +33,6 @@ interface IPageContentData {
       body: string;
     };
     frontmatter: {
-      tags: string[];
       title: string;
     };
   };
@@ -53,27 +48,7 @@ const PortfolioPage: React.FunctionComponent<{
   return (
     <Layout>
       <SEO title={page.frontmatter.title} />
-
-      <div className="pt3 pb1">
-        <Link className="link f2 fw2 accent sans" to={pageContext.slug}>
-          {page.frontmatter.title}
-        </Link>
-      </div>
-      <div className="pt2 f5">
-        <span className="sans accent-mono pr2">Associated posts:</span>
-        {page.frontmatter.tags.map(tag => {
-          return (
-            <>
-              <Link className="link accent-mono" to={`/tags/posts/${tag}`}>
-                <Icon sprite={TagIcon} className="w1 h1 v-mid" />
-                {` `}
-                {tag}
-              </Link>
-            </>
-          );
-        })}
-      </div>
-      <div className="center mw7 tl lh-copy ph2 content">
+      <div className="center tl lh-copy content portfolio-container sans">
         <MDXRenderer>{page.code.body}</MDXRenderer>
       </div>
     </Layout>

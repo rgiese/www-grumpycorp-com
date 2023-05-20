07c7725ac31006d83760e2f02b37e65d00cd083c
Robin Giese <robin@grumpycorp.com>
2023-05-20 16:04:55 -0700

    Renaming 'input' directory to 'content'

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿return await Bootstrapper.Factory
     .CreateWeb(args)
+    // Input settings
+    .AddSetting(WebKeys.InputPaths, "content")
     // Output settings
     .AddSetting(Keys.Host, new Uri("https://grumpycorp.com").Host)
     .AddSetting(Keys.LinksUseHttps, true)

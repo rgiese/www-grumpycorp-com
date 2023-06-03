15d72fd273642f3496b082046f48c37508588561
Robin Giese <robin@grumpycorp.com>
2023-06-02 19:42:40 -0700

    Basic exploration

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Markdig;
+
+Ingredient myDoc = new Ingredient
+{
+    Children =
+    {
+        new HeadingIngredient(1, "Heading"),
+        new HeadingIngredient(2)
+        {
+            Children = { new TextIngredient("Subheading"), new TextIngredient("More stuff") }
+        },
+        new DivIngredient
+        {
+            Children =
+            {
+                new TextIngredient("Hello"),
+                new ItalicIngredient { Children = { new TextIngredient("World") } },
+            }
+        },
+        new DivIngredient(null, "SomeText"),
+        new DivIngredient("w100", "SomeText"),
+        new UnorderedListIngredient
+        {
+            Children = { new TextIngredient("Item1"), new TextIngredient("Item2") }
+        }
+    }
+};
+
+//Console.WriteLine(myDoc.ToHtml());
+
+var markdownPipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+
+var markdownDocument = Markdown.Parse("Foo *bar*", markdownPipeline);
+
+Console.WriteLine(markdownDocument.ToString());
